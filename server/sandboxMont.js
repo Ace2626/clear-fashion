9a454b647c1158db9e99b340b4a933ccb88e1329
Ace2626 <96004298+Ace2626@users.noreply.github.com>
2022-02-13 12:33:53 +0100

    TD4_EssaisScrapping

---
+++ server/sandboxMont.js
@@ -0,0 +1,22 @@
+const dedicatedbrand = require('./sources/montlimartbrand.js');
+
+async function sandbox (eshop = 'https://www.montlimart.com/pulls-sweats.html') {
+  try {
+    
+    console.log(`🕵️‍♀️  browsing ${eshop} source`);
+
+    var products = await dedicatedbrand.scrape(eshop);
+    
+
+    console.log(products);  
+    console.log('done');
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+const [,, eshop] = process.argv;
+
+sandbox(eshop);
--- server/sources/montlimartbrand.js
+++ server/sources/montlimartbrand.js
@@ -9,7 +9,7 @@ const cheerio = require('cheerio');
 const parse = data => {
   const $ = cheerio.load(data);
 
-  return $('.category-products .product')
+  return $('.category-products.item')
     .map((i, element) => {
       const name = $(element)
         .find('.product-name')
