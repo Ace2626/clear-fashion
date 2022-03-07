169114ed88cba6286f9ce056f9f1a8b2d9205fe7
Ace2626 <96004298+Ace2626@users.noreply.github.com>
2022-03-07 09:17:41 +0100

    TD5#3

--- server/storage.js
+++ server/storage.js
@@ -4,11 +4,15 @@ const {MongoClient} = require('mongodb');
 const MONGODB_URI = 'mongodb+srv://<AntoineS>:<mStarWars911>@<cluster-url>?retryWrites=true&writeConcern=majority';
 const MONGODB_DB_NAME = 'Cluster0';
 
-const client = MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
-const db =  client.db(MONGODB_DB_NAME)
+//const client = MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+//const db =  client.db(MONGODB_DB_NAME)
+MongoClient.connect(MONGODB_URI, (error, client) => {
+    const db =  client.db(MONGODB_DB_NAME)
+    const products = JSON.parse(fs.readFileSync('products.json'));
+    const collection = db.collection('products');
+    const result = collection.insertMany(products)
+    console.log(result);})
 
-const products = JSON.parse(fs.readFileSync(products.json));
 
-const result = collection.insertMany(products);
 
-console.log(result);
+
